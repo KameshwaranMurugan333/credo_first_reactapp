8d1c60ddf59d99a8fd83119e5f45c1c5e55a6144
Kameshwaran Murugan <kameshwaran@crayond.co>
2022-08-14 09:19:34 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import './App.css';
 import React from 'react';
-import { ClassComp, LoginScreen } from './screens';
-import { ThemeContext } from './contexts';
+import { LoginScreen } from './screens';
 
 function App() {
-
-  const [state, setState] = React.useState({ message: "I am in App.js" });
-  const [theme, setTheme] = React.useState({ currentTheme: 'light' });
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={()=>setTheme({...theme,currentTheme: theme.currentTheme === "light" ? "dark" : "light"})} >Toogle Theme</button>
-        <ThemeContext.Provider value={theme}>
-          <ClassComp />
           <LoginScreen />
-        </ThemeContext.Provider>
       </header>
     </div>
   );
